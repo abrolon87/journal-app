bc9a78c56cd7bfbf822dadb6a53227617046216b
Amanda Rolon <abrolon87@gmail.com>
2021-02-22 16:34:40 -0500

    builds topics function

--- journal_entries/views.py
+++ journal_entries/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
@@ -6,3 +7,10 @@ from django.shortcuts import render
 def index(request):
     # home page
     return render(request, 'journal_entries/index.html')
+
+
+def topics(request):
+    # all topics
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'journal_entries/topics.html', context)

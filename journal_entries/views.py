1a2311ef1187bccb137b48bc39fcb4be48395415
Amanda Rolon <abrolon87@gmail.com>
2021-02-25 18:52:21 -0500

    imports redirect and TopicForm; builds new_topic view

--- journal_entries/views.py
+++ journal_entries/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 
@@ -22,3 +23,15 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'journal_entries/topic.html', context)
+
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            return redirect('journal_entries:topics')
+
+    context = {'form': form}
+    return render(request, 'journal_entries/new_topic.html', context)

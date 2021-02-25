1b7b87c72e692ac3957fc3392809cbcec6647608
Amanda Rolon <abrolon87@gmail.com>
2021-02-25 18:49:31 -0500

    adds new topic path to urlpatterns

--- journal_entries/urls.py
+++ journal_entries/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     # topics page
     path('topics/', views.topics, name='topics'),
     # individual topic page
-    path('topics/<int:topic_id>/', views.topic, name='topic')
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # new topic page
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]

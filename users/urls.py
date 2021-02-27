a792aff2bc4dc02f47d074f00f2d8b5f43d4bdc2
Amanda Rolon <abrolon87@gmail.com>
2021-02-26 20:52:33 -0500

    adds register path to urlpatterns

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 app_name = 'users'
 
 urlpatterns = [
+    # includes default auth urls
     path('', include('django.contrib.auth.urls')),
+    # registration page
+    path('register/', views.register, name='register'),
 ]

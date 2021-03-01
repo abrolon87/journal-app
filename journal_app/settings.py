a403260bf7241e9325ad172a91caaf7f6f345264
Amanda Rolon <abrolon87@gmail.com>
2021-02-28 19:40:25 -0500

    adds bootstrap; applies bootstrap to base.html

--- journal_app/settings.py
+++ journal_app/settings.py
@@ -31,8 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # my apps
     'journal_entries',
     'users',
+    # third party apps
+    'bootstrap4',
+    # default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
